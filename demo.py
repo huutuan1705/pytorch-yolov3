3c2e5aa817d20ad5f1e7d19422a6f52cc99c72c0
tuan.nh <tuan.nh@tinasoft.com.vn>
2024-10-24 15:42:41 +0700

    add demo

---
+++ demo.py
@@ -0,0 +1,58 @@
+from __future__ import division
+
+import os
+import argparse
+import numpy as np
+import torch
+import cv2
+
+from pytorchyolo.models import load_model
+from pytorchyolo.utils.utils import to_cpu, load_classes, print_environment_info, provide_determinism, worker_seed_set
+from pytorchyolo.utils.parse_config import parse_data_config
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def predict(image_path, model):
+    # Đọc ảnh và chuyển thành tensor
+    image = cv2.imread(image_path)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_tensor = torch.tensor(image_rgb).permute(2, 0, 1).float().to(device) / 255.0
+    image_tensor = image_tensor.unsqueeze(0)  # Thêm batch dimension
+    
+    # Chuyển mô hình sang chế độ eval và tắt tính toán gradient
+    model.eval()
+    with torch.no_grad():
+        predictions = model(image_tensor)
+    
+    return predictions
+
+def run():
+    parser = argparse.ArgumentParser(description="Trains the YOLO model.")
+    parser.add_argument("-m", "--model", type=str, default="config/yolov3.cfg", help="Path to model definition file (.cfg)")
+    parser.add_argument("-d", "--data", type=str, default="config/coco.data", help="Path to data config file (.data)")
+    parser.add_argument("-e", "--epochs", type=int, default=300, help="Number of epochs")
+    parser.add_argument("-v", "--verbose", action='store_true', help="Makes the training more verbose")
+    parser.add_argument("--n_cpu", type=int, default=8, help="Number of cpu threads to use during batch generation")
+    parser.add_argument("--pretrained_weights", type=str, help="Path to checkpoint file (.weights or .pth). Starts training from checkpoint model")
+    parser.add_argument("--checkpoint_interval", type=int, default=1, help="Interval of epochs between saving model weights")
+    parser.add_argument("--evaluation_interval", type=int, default=1, help="Interval of epochs between evaluations on validation set")
+    parser.add_argument("--multiscale_training", action="store_true", help="Allow multi-scale training")
+    parser.add_argument("--iou_thres", type=float, default=0.5, help="Evaluation: IOU threshold required to qualify as detected")
+    parser.add_argument("--conf_thres", type=float, default=0.1, help="Evaluation: Object confidence threshold")
+    parser.add_argument("--nms_thres", type=float, default=0.5, help="Evaluation: IOU threshold for non-maximum suppression")
+    parser.add_argument("--logdir", type=str, default="logs", help="Directory for training log files (e.g. for TensorBoard)")
+    parser.add_argument("--seed", type=int, default=-1, help="Makes results reproducable. Set -1 to disable.")
+    parser.add_argument("--image_path", type=str, help="Path to image")
+    args = parser.parse_args()
+    
+    data_config = parse_data_config(args.data)
+    train_path = data_config["train"]
+    valid_path = data_config["valid"]
+    class_names = load_classes(data_config["names"])
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    model = load_model(args.model, args.pretrained_weights)
+    
+    predictions = predict(args.image_path, model)
+    
+    print(predictions)
